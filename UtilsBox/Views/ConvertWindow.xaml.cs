f3b71865179eb817c59a3b2abd5fc01a1baf8a17
caoDenk <caodenk@outlook.com>
2022-11-22 09:37:26 +0800

    edit

---
+++ UtilsBox/Views/ConvertWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UtilsBox.Views
+{
+    /// <summary>
+    /// ConvertWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class ConvertWindow : Window
+    {
+        public ConvertWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string v = Input.Text;
+            //MessageBox.Show(v);
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (char c in v)
+            {
+                if (c == '\\')
+                {
+                    stringBuilder.Append('/');
+                }
+                else
+                    stringBuilder.Append(c);
+            }
+            Result.Text = stringBuilder.ToString();
+            Display.Text = "转换结果已经保存在截切版里";
+            Clipboard.SetText(Result.Text);
+        }
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            Input.Clear();
+            Result.Clear();
+            Display.Text = "";
+        }
+
+        //protected override void OnClosed(EventArgs e)
+        //{
+
+        //    base.OnClosed(e);
+
+        //}
+      
+    }
+}
